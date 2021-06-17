8491c0bc70b87a6bd02d7b28e63140f7abb2657d
Dao Pham <daopm.dean@gmail.com>
2021-06-17 11:59:20 +0700

    Added: Manager Controller

--- STS/Controllers/ManagerController.cs
+++ STS/Controllers/ManagerController.cs
@@ -1,21 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Data.Models.Requests;
+using Data.Models.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Service.Enums;
+using Service.Exceptions;
+using Service.Interfaces;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace STS.Controllers
 {
     [Authorize(Policy = "RequiredManagers")]
-    [Route("api/admin")]
+    [Route("api/manager")]
     public class ManagerController : ApiBaseController
     {
-        public ManagerController()
-        {
+        private readonly IUserService _userService;
+        private readonly IAuthService _authService;
 
+        public ManagerController(
+            IUserService userService,
+            IAuthService authService)
+        {
+            _userService = userService;
+            _authService = authService;
+        }
+
+        [HttpPost("users/store-manager")]
+        public async Task<IActionResult> RegisterStoreManager(RegisterRequest info)
+        {
+            try
+            {
+                return Ok(await _authService
+                    .RegisterWithRole((int)UserRole.StoreManager, info));
+            }
+            catch (AppException appEx)
+            {
+                return BadRequest(new ErrorResponse
+                {
+                    StatusCode = appEx.StatusCode,
+                    Message = appEx.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ErrorResponse
+                {
+                    StatusCode = 500,
+                    Message = ex.Message,
+                    StackTrace = ex.StackTrace
+                });
+            }
+        }
+
+        [HttpPost("users/staff")]
+        public async Task<IActionResult> RegisterStaff(RegisterRequest info)
+        {
+            try
+            {
+                return Ok(await _authService
+                    .RegisterWithRole((int)UserRole.Staff, info));
+            }
+            catch (AppException appEx)
+            {
+                return BadRequest(new ErrorResponse
+                {
+                    StatusCode = appEx.StatusCode,
+                    Message = appEx.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ErrorResponse
+                {
+                    StatusCode = 500,
+                    Message = ex.Message,
+                    StackTrace = ex.StackTrace
+                });
+            }
         }
     }
 }
