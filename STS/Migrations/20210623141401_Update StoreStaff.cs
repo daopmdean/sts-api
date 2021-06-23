cc05a2a30486a7e82c2d9878a6c70d2d56915c4a
Dao Pham <daopm.dean@gmail.com>
2021-06-23 21:41:02 +0700

    Modified: Migrations

---
+++ STS/Migrations/20210623141401_Update StoreStaff.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace STS.Migrations
+{
+    public partial class UpdateStoreStaff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsPrimaryStaff",
+                table: "StoreStaffs",
+                newName: "IsPrimaryStore");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsPrimaryStore",
+                table: "StoreStaffs",
+                newName: "IsPrimaryStaff");
+        }
+    }
+}
