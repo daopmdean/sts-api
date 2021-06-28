17c1e1bd9c180fb7dffa3cc0c25dfa66e01341b8
Dao Pham <daopm.dean@gmail.com>
2021-06-28 10:14:52 +0700

    Modified: StoreStaffOverview

--- Data/Models/Responses/StoreStaffOverview.cs
+++ Data/Models/Responses/StoreStaffOverview.cs
@@ -3,8 +3,14 @@ namespace Data.Models.Responses
 {
     public class StoreStaffOverview
     {
-        public StoreStaffOverview()
-        {
-        }
+        public int StoreId { get; set; }
+
+        public string Username { get; set; }
+
+        public bool IsManager { get; set; }
+
+        public bool IsPrimaryStore { get; set; }
+
+        public DateTime DateStart { get; set; }
     }
 }
