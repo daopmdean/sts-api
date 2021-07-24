80eef68e08798d754d5c234e0637892c236d0d17
DESKTOP-VT56IRQ\daopm <daopm.dean@gmail.com>
2021-07-24 17:09:23 +0700

    Modified: publish shift assignment

--- Service/Implementations/ShiftAssignmentService.cs
+++ Service/Implementations/ShiftAssignmentService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Data.Entities;
+using Data.Enums;
 using Data.Models.Requests;
 using Data.Models.Responses;
 using Data.Pagings;
 using Data.Repositories.Interfaces;
+using Service.Enums;
 using Service.Exceptions;
 using Service.Interfaces;
 
@@ -18,6 +20,7 @@ namespace Service.Implementations
         private readonly IUserRepository _userRepo;
         private readonly IStoreRepository _storeRepo;
         private readonly ISkillRepository _skillRepo;
+        private readonly IWeekScheduleRepository _weekScheduleRepo;
         private readonly IMapper _mapper;
 
         public ShiftAssignmentService(
@@ -25,18 +28,46 @@ namespace Service.Implementations
             IUserRepository userRepo,
             IStoreRepository storeRepo,
             ISkillRepository skillRepo,
+            IWeekScheduleRepository weekScheduleRepo,
             IMapper mapper)
         {
             _shiftAssignmentRepo = shiftAssignmentRepo;
             _userRepo = userRepo;
             _storeRepo = storeRepo;
             _skillRepo = skillRepo;
+            _weekScheduleRepo = weekScheduleRepo;
             _mapper = mapper;
         }
 
         public async Task<ShiftAssignment> CreateShiftAssignment(
             ShiftAssignmentCreate create)
         {
+            var weekSchedule = await _weekScheduleRepo
+                .GetByIdAsync(create.WeekScheduleId);
+
+            if (weekSchedule == null)
+                throw new AppException((int)StatusCode.BadRequest, 
+                    "WeekSchedule not found");
+
+            if (weekSchedule.Status != Status.Unpublished)
+                throw new AppException((int)StatusCode.BadRequest, 
+                    "Can only publish unpublished week schedule");
+
+            var publishedWeekSchedule = await _weekScheduleRepo
+                .GetWeekSchedulesAsync(weekSchedule.StoreId, weekSchedule.DateStart, Status.Published);
+
+            if (publishedWeekSchedule == null)
+            {
+                weekSchedule.Status = Status.Published;
+            }
+            else
+            {
+                foreach (var schedule in publishedWeekSchedule)
+                {
+                    schedule.Status = Status.Unpublished;
+                }
+            }
+
             foreach (var shift in create.ShiftAssignments)
             {
                 var store = await _storeRepo
@@ -61,6 +92,7 @@ namespace Service.Implementations
                         "Conflicted with the FOREIGN KEY constraint, Username does not exist");
 
                 var shiftAssignment = _mapper.Map<ShiftAssignment>(shift);
+                shiftAssignment.WeekScheduleId = create.WeekScheduleId;
                 await _shiftAssignmentRepo.CreateAsync(shiftAssignment);
             }
             
