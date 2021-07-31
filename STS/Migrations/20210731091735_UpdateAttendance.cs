d559bfd89994d58d580f70305bd8aaceb987f30f
Dao Pham <daopm.dean@gmail.com>
2021-07-31 16:33:38 +0700

    Modified: remove shift attendance

---
+++ STS/Migrations/20210731091735_UpdateAttendance.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace STS.Migrations
+{
+    public partial class UpdateAttendance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShiftAttendances_ShiftAssignments_ShiftAssignmentId",
+                table: "ShiftAttendances");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ShiftAssignmentId",
+                table: "ShiftAttendances",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShiftAssignmentId1",
+                table: "ShiftAttendances",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Attendances",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShiftAttendances_ShiftAssignmentId1",
+                table: "ShiftAttendances",
+                column: "ShiftAssignmentId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShiftAttendances_ShiftAssignments_ShiftAssignmentId1",
+                table: "ShiftAttendances",
+                column: "ShiftAssignmentId1",
+                principalTable: "ShiftAssignments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShiftAttendances_ShiftAssignments_ShiftAssignmentId1",
+                table: "ShiftAttendances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShiftAttendances_ShiftAssignmentId1",
+                table: "ShiftAttendances");
+
+            migrationBuilder.DropColumn(
+                name: "ShiftAssignmentId1",
+                table: "ShiftAttendances");
+
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Attendances");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ShiftAssignmentId",
+                table: "ShiftAttendances",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShiftAttendances_ShiftAssignments_ShiftAssignmentId",
+                table: "ShiftAttendances",
+                column: "ShiftAssignmentId",
+                principalTable: "ShiftAssignments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ STS/Migrations/20210731092844_UpdateAttendance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace STS.Migrations
+{
+    public partial class UpdateAttendance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShiftAttendances");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Attendances",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Attendances");
+
+            migrationBuilder.CreateTable(
+                name: "ShiftAttendances",
+                columns: table => new
+                {
+                    ShiftAssignmentId = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    TimeCheckIn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    TimeCheckOut = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShiftAttendances", x => x.ShiftAssignmentId);
+                    table.ForeignKey(
+                        name: "FK_ShiftAttendances_ShiftAssignments_ShiftAssignmentId",
+                        column: x => x.ShiftAssignmentId,
+                        principalTable: "ShiftAssignments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+    }
+}
